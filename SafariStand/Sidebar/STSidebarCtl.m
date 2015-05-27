f3fcdce636ac963199bd3d3e3034421a76c01cb2
hetima <hetima@hetima.com>
2015-05-27 11:15:56 +0900

    reduce frequency of layout

--- SafariStand/Sidebar/STSidebarCtl.m
+++ SafariStand/Sidebar/STSidebarCtl.m
@@ -123,7 +123,8 @@
 
 - (void)tabViewItemSelected:(NSNotification*)note
 {
-    [self layout:[self rightSide]];
+    //以前はその都度調整しないとレイアウト崩れる気がしてたんだけど、大丈夫みたいなんで何もしないテスト
+    //[self layout:[self rightSide]];
 }
 
 
