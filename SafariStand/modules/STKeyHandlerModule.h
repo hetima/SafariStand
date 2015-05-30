007c9d362d466c8a5e5766cf6bb7b12c18e23a7d
hetima <hetima@hetima.com>
2015-05-30 14:52:53 +0900

    remove menu. use handler

--- SafariStand/modules/STKeyHandlerModule.h
+++ SafariStand/modules/STKeyHandlerModule.h
@@ -7,11 +7,5 @@
 
 @interface STKeyHandlerModule : STCModule
 
-- (void)prefValue:(NSString*)key changed:(id)value;
-
-//,.
-- (void)setupOneKeyNavigationMenuItem;
-- (void)insertOneKeyNavigationMenuItem;
-- (void)removeOneKeyNavigationMenuItem;
 
 @end
--- SafariStand/modules/STKeyHandlerModule.m
+++ SafariStand/modules/STKeyHandlerModule.m
@@ -19,14 +19,6 @@
     self = [super initWithStand:core];
     if (!self) return nil;
     
-    
-    [self setupOneKeyNavigationMenuItem];
-    BOOL enabled=[[STCSafariStandCore ud]boolForKey:kpSwitchTabWithOneKeyEnabled];
-    [self setupTabNavigationMenuItem:enabled];
-    
-    
-    [self observePrefValue:kpSwitchTabWithOneKeyEnabled];
-    
     //Intercept cmd+num
     KZRMETHOD_SWIZZLING_("BookmarksController", "goToNthFavoriteLeaf:", void, call, sel)
     ^(id slf, int arg1)
@@ -39,7 +31,30 @@
         
     }_WITHBLOCK;
     
-    
+    //switch tab with ,. 
+    [core registerBrowserWindowKeyDownHandler:^BOOL(NSEvent *event, NSWindow* window) {
+
+        if ([[STCSafariStandCore ud]boolForKey:kpSwitchTabWithOneKeyEnabled]) {
+            unsigned short key=[event keyCode];
+            NSEventModifierFlags flag=([event modifierFlags] & NSDeviceIndependentModifierFlagsMask);
+            SEL cmd=nil;
+            
+            if (flag && flag!=NSControlKeyMask) {
+                return NO;
+            }
+            if(key==0x2B||key==0x5F){//kVK_ANSI_Comma kVK_JIS_KeypadComma
+                cmd=@selector(selectPreviousTab:);
+            }else if(key==0x2F||key==0x41){//kVK_ANSI_Period kVK_ANSI_KeypadDecimal
+                cmd=@selector(selectNextTab:);
+            }
+            
+            if (cmd) {
+                [NSApp sendAction:cmd to:nil from:nil];
+                return YES;
+            }
+        }
+        return NO;
+    }];
     
     return self;
 }
@@ -47,79 +62,10 @@
 
 - (void)prefValue:(NSString*)key changed:(id)value
 {
-    if([key isEqualToString:kpSwitchTabWithOneKeyEnabled]){
-        //BOOL enabled=[[STCSafariStandCore ud]boolForKey:kpSwitchTabWithOneKeyEnabled];
-        BOOL enabled=[value boolValue];
-        [self setupTabNavigationMenuItem:enabled];
-        //if(enabled)[self insertOneKeyNavigationMenuItem];
-        //else [self removeOneKeyNavigationMenuItem];
-    }
+
 }
 
 
-- (void)setupOneKeyNavigationMenuItem
-{
-    NSMenu* subMenu=[[NSMenu alloc]initWithTitle:@"Navigation"];
-    _oneKeyNavigationMenuItem=[[NSMenuItem alloc]initWithTitle:@"Navigation" action:nil keyEquivalent:@""];
-    //[_oneKeyNavigationMenuItem setHidden:YES];
-    
-    id m;
-    m=[subMenu addItemWithTitle:@"selectPreviousTab" action:@selector(selectPreviousTab:) keyEquivalent:@""];
-    [m setKeyEquivalentModifierMask:0];
-    [m setTag:kMenuItemTagSelectPreviousTab];
-    m=[subMenu addItemWithTitle:@"selectNextTab" action:@selector(selectNextTab:) keyEquivalent:@""];
-    [m setKeyEquivalentModifierMask:0];
-    [m setTag:kMenuItemTagSelectNextTab];
-
-    
-    [_oneKeyNavigationMenuItem setSubmenu:subMenu];
-
-    [_oneKeyNavigationMenuItem setTag:kMenuItemTagOneKeyNavigation];
-    
-    //insert
-    NSMenu* standMenu=[STCSafariStandCore si].standMenu;
-    id toRemove=[standMenu itemWithTag:kMenuItemTagOneKeyNavigation];
-    if(!toRemove){
-        [[STCSafariStandCore si]addItemToStandMenu:_oneKeyNavigationMenuItem];
-    }
-}
-
-
-- (void)insertOneKeyNavigationMenuItem
-{
-    NSMenu* standMenu=[STCSafariStandCore si].standMenu;
-    id toRemove=[standMenu itemWithTag:kMenuItemTagOneKeyNavigation];
-    if(!toRemove){
-        [[STCSafariStandCore si]addItemToStandMenu:_oneKeyNavigationMenuItem];
-    }
-}
-
-
-- (void)removeOneKeyNavigationMenuItem
-{
-    NSMenu* standMenu=[STCSafariStandCore si].standMenu;
-    id toRemove=[standMenu itemWithTag:kMenuItemTagOneKeyNavigation];
-    if(toRemove){
-        [standMenu removeItem:toRemove];
-    }
-}
-
-
-- (void)setupTabNavigationMenuItem:(BOOL)enabled
-{
-    NSMenuItem* m;
-
-    m=[[_oneKeyNavigationMenuItem submenu]itemWithTag:kMenuItemTagSelectPreviousTab];
-    if (m) {
-        if (enabled) [m setKeyEquivalent:@","];
-        else  [m setKeyEquivalent:@""];
-    }
-    m=[[_oneKeyNavigationMenuItem submenu]itemWithTag:kMenuItemTagSelectNextTab];
-    if (m) {
-        if (enabled) [m setKeyEquivalent:@"."];
-        else  [m setKeyEquivalent:@""];
-    }
-}
 
 #pragma mark - Intercept cmd+num
 
